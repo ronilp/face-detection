f807bae6c4d2ca42f408afdbff1f60cc241f9f20
Ronil-mbp <rpancho@ncsu.edu>
2019-04-06 02:05:09 -0400

    config and dataset utils

---
+++ config.py
@@ -0,0 +1,26 @@
+# File: config.py
+# Author: Ronil Pancholia
+# Date: 4/6/19
+# Time: 2:04 AM
+
+import torch
+
+### Learning Parameters
+BASE_LR = 1e-4
+TRAIN_EPOCHS = 50
+EARLY_STOPPING_ENABLED = False
+EARLY_STOPPING_PATIENCE = 10
+
+### Dataset Config
+DATA_DIR = "data"
+ALLOWED_CLASSES = []
+NUM_CLASSES = len(ALLOWED_CLASSES)
+
+### Miscellaneous Config
+MODEL_PREFIX = "model_name"
+BATCH_SIZE = 16
+RANDOM_SEED = 629
+
+### GPU SETTINGS
+CUDA_DEVICE = 0  # GPU device ID
+GPU_MODE = torch.cuda.is_available()
---
+++ dataset_utils.py
@@ -0,0 +1,40 @@
+# File: dataset_utils.py
+# Author: Ronil Pancholia
+# Date: 4/6/19
+# Time: 2:03 AM
+
+import os
+import torch
+import multiprocessing
+
+import config
+
+
+def load_datasets(DatasetClass, transforms=None, datasets=None):
+    if datasets is None:
+        datasets = {x: DatasetClass(os.path.join(config.DATA_DIR, x), x, transforms) for x in ['train', 'val']}
+    dataset_loaders = {
+        x: torch.utils.data.DataLoader(datasets[x], batch_size=config.BATCH_SIZE, shuffle=True,
+                                       num_workers=multiprocessing.cpu_count()) for x in ['train', 'val']}
+    dataset_sizes = {x: len(datasets[x]) for x in ['train', 'val']}
+    return dataset_loaders, dataset_sizes
+
+
+def load_testset(DatasetClass, transforms=None, datasets=None):
+    if datasets is None:
+        datasets = {x: DatasetClass(os.path.join(config.DATA_DIR, x), x, transforms) for x in ['test']}
+    dataset_loaders = {
+        x: torch.utils.data.DataLoader(datasets[x], batch_size=config.BATCH_SIZE * 4, shuffle=False,
+                                       num_workers=multiprocessing.cpu_count()) for x in ['test']}
+    dataset_sizes = {x: len(datasets[x]) for x in ['test']}
+    return dataset_loaders, dataset_sizes
+
+
+def load_datasets_from_csv(DatasetClass, transforms=None):
+    datasets = {x: DatasetClass(config.DATA_DIR, x, transforms) for x in ['train', 'val']}
+    return load_datasets(DatasetClass, transforms, datasets)
+
+
+def load_testset_from_csv(DatasetClass, transforms=None):
+    datasets = {x: DatasetClass(config.DATA_DIR, x, transforms) for x in ['test']}
+    return load_testset(DatasetClass, transforms, datasets)
